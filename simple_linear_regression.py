65c084564cacd5008d38be053ea2619dbfaf37c1
Rene Nespithal <rene.nespithal@gmail.com>
2019-01-30 21:46:08 +0100

    Create simple_linear_regression.py
    import libraries and dataset

---
+++ simple_linear_regression.py
@@ -0,0 +1,11 @@
+# Simple Linear Regression
+
+# Import the libraries
+import numpy as np
+import matplotlib.pylot as plt
+import pandas as pd
+
+# Import the dataset
+dataset = pd.read_csv('Salary_Data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 3].values
