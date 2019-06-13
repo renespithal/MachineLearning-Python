b909b63a85c887bf5bbc81274c1a338bfee65246
Rene Nespithal <rene.nespithal@gmail.com>
2019-06-13 13:11:30 +0200

    Update data_preprocessing.py

--- data_preprocessing/data_preprocessing.py
+++ data_preprocessing/data_preprocessing.py
@@ -2,10 +2,14 @@
 
 # Import the libraries
 import numpy as np
-import matplotlib.pylot as plt
+import matplotlib.pyplot as plt
 import pandas as pd
 
 # Import the dataset
 dataset = pd.read_csv('Salary_Data.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 3].values
+
+# Taking care of missing data
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
